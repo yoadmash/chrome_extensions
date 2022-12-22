8d8943aab352a9fd55b82c76918ab6f88e3400f4
yoadmash <yoad.mashiach2@gmail.com>
2022-12-22 18:05:59 +0200

    progress

--- whatsapp chrome extension/script.js
+++ whatsapp chrome extension/script.js
@@ -1,21 +1,30 @@
 window.onload = () => {
     const input = document.querySelector('input');
     const btn = document.querySelector('button');
+    let tab1 = {};
 
     function openChat() {
         if (input.value.length === 10 && !isNaN(input.value)) {
             const formated = input.value.slice(1, input.value.length);
             const url = `https://api.whatsapp.com/send/?phone=972${formated}&text&type=phone_number&app_absent=1`;
             chrome.tabs.create({
-                url: url
+                url: url,
+                active: false
             });
+            setTimeout(async () => {
+                await chrome.tabs.query({url: 'https://web.whatsapp.com/'}, (tabs) => {
+                    // chrome.tabs.sendMessage(tabs[tabs.length-1].id, {msg: 'test'});
+                    tab1 = tabs;
+                    console.log(tab1);
+                })
+            }, 1000);
         }
         else {
             input.value = '';
             input.placeholder = 'Error: wrong number format!';
             setTimeout(() => {
                 input.placeholder = 'Required format: 0521234567';
-            },1000);
+            }, 1000);
         }
     }
 
