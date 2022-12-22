e6598a48b06c30ab518541058152a702769c725a
yoadmash <yoad.mashiach2@gmail.com>
2022-12-22 23:51:16 +0200

    final version

--- whatsapp chrome extension/checkConnection.js
+++ whatsapp chrome extension/checkConnection.js
@@ -7,18 +7,32 @@
         else reopen the tab with the number provided by input.value
 */
 
-const body = document.body;
 setTimeout(() => {
+    const body = document.body;
     const qrCode = document.getElementsByClassName("_2UwZ_")[0];
     if (!body.contains(qrCode)) {
-        document.getElementsByClassName('_3GlyB dwbWf')[0].click();
-        const phoneNumber = document.getElementsByClassName('erpdyial i0jNr selectable-text copyable-text')[0].innerHTML;
-        if(body.contains(phoneNumber) && phoneNumber.innerHTML.includes('+972 50-989-4366')) {
-            
-        } else {
-            console.log('error: not a business account')
-        }
+
+        const buttons = document.getElementsByClassName('_1QVfy _3UaCz')[0].getElementsByTagName('span')[0];
+        const inputButton = document.createElement('div');
+        inputButton.innerHTML = '📱';
+        inputButton.style.fontSize = '24px';
+        inputButton.style.cursor = 'pointer';
+        inputButton.title = 'New chat by phone number';
+
+        inputButton.addEventListener("click", () => {
+            let number = prompt("Required format: 0521234567");
+            if (number != null && number.length === 10 && !isNaN(number)) {
+                const formatted = number.slice(1, number.length);
+                const url = `https://api.whatsapp.com/send/?phone=972${formatted}&text&type=phone_number&app_absent=1`;
+                window.open(url, "_self");
+            } else {
+                alert('Error: wrong number format!');
+            }
+        })
+
+        buttons.prepend(inputButton);
+
     } else {
-        console.log('error: missing whatsapp web connection');
+        alert('Attention: In order for the WhatsApp extension to work, please connect and reload the page.');
     }
-}, 3000);
+}, 5000);
