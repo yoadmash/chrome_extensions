aae12cf684c3f199c52016dd941a7fb9acf2e8a4
yoadmash <yoad.mashiach2@gmail.com>
2022-12-23 12:46:50 +0200

    saving

--- whatsapp chrome extension/whatsAppInjection.js
+++ whatsapp chrome extension/whatsAppInjection.js
@@ -1,9 +1,11 @@
+import './bootstrap/bootstrap.min.css';
+
 setTimeout(() => {
     const body = document.body;
     const qrCode = document.getElementsByClassName("_2UwZ_")[0];
     if (!body.contains(qrCode)) {
-
         const buttons = document.getElementsByClassName('_1QVfy _3UaCz')[0].getElementsByTagName('span')[0];
+
         const inputButton = document.createElement('div');
         inputButton.innerHTML = '📱';
         inputButton.style.fontSize = '24px';
@@ -12,16 +14,15 @@ setTimeout(() => {
 
         inputButton.addEventListener("click", () => {
             let number = prompt("Required format: 0521234567");
-            if (number != null && number.length === 10 && !isNaN(number)) {
+            if (number !== null && number.length === 10 && !isNaN(number)) {
                 const formatted = number.slice(1, number.length);
                 const url = `https://api.whatsapp.com/send/?phone=972${formatted}&text&type=phone_number&app_absent=1`;
                 window.open(url, "_self");
             }
         })
-
+        
         buttons.prepend(inputButton);
-
     } else {
-        alert('Attention: In order for the WhatsApp extension to work, please connect and reload the page.');
+        alert('Attention: In order for this WhatsApp extension to work, please link your device and reload the page.');
     }
 }, 5000);
