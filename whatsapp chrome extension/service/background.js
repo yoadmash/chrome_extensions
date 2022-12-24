1ab6192f0d2cda533da2a4b815862bbb1184a47a
yoadmash <yoad.mashiach2@gmail.com>
2022-12-25 00:34:43 +0200

    final touches

--- whatsapp chrome extension/service/background.js
+++ whatsapp chrome extension/service/background.js
@@ -4,5 +4,5 @@ chrome.contentSettings.javascript.set({
 }, console.warn("api.whatsapp.com: JavaScript is blocked in order to prevent WhatsApp from launching the desktop application as well (via chrome extension)"));
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.set({whatsapp_extension: []});
+    chrome.storage.local.set({whatsapp_extension: ['0524616809','0523010317','0522210460','0542112871', '0525306042', '0521234567']});
 })
