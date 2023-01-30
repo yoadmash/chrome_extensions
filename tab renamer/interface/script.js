dceda3eb52c84afbb75d418c974dd9b7a5a41d9d
yoadmash <yoad.mashiach2@gmail.com>
2023-01-30 14:29:26 +0200

    foundation for automatic title

--- tab renamer/interface/script.js
+++ tab renamer/interface/script.js
@@ -117,6 +117,11 @@ function renderWindowTabs(window) {
 
 function setTitle(newTitle) {
     document.title = newTitle;
+    // let input = undefined;
+    // while(!input) {
+    //     input = Array.from(document.querySelectorAll('input')).find(element => element.placeholder === 'שם העסק');
+    // }
+    // document.title = input.value;
 }
 
 function search(windows) {
