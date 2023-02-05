84e9df257d634fc9ee3354da31053bb7afe2f717
yoadmash <yoad.mashiach2@gmail.com>
2023-02-05 16:54:56 +0200

    automatic title

--- tab renamer/service/background.js
+++ tab renamer/service/background.js
@@ -1,33 +1,30 @@
-// function setTitle(editMode) {
-//     if(editMode) {
-//         const checkField = setInterval(() => {
-//             let input = Array.from(document.querySelectorAll('input')).find(element => element.placeholder === 'שם העסק');
-//             if (input && input.value.length > 0) {
-//                 document.title = input.value;
-//                 clearInterval(checkField);
-//             }
-//         }, 250);
-//     } else {
-//         defaultTitle();
-//     }
-// }
+function setTitle() {
+    if (document.URL.match('https://crm.corecrm.co/companies/*')) {
+        const urlArr = Array.from(document.URL);
+        if (urlArr.slice(33, 69).join('').length === 36) {
+            const editView = document.URL.includes('edit');
+            if (editView) {
+                const checkField = setInterval(() => {
+                    let input = Array.from(document.querySelectorAll('input')).find(element => element.placeholder === 'שם העסק');
+                    if (input && input.value.length > 0) {
+                        document.title = input.value;
+                        clearInterval(checkField);
+                    }
+                }, 250);
+            }
+        } else {
+            document.title = 'Pay Plus Admin';
+        }
+    } else {
+        document.title = 'Pay Plus Admin';
+    }
+}
 
-// function defaultTitle() {
-//     document.title = 'Pay Plus Admin';
-// }
-
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//     if (changeInfo.url) {
-//         if (tab.url.match('https://crm.corecrm.co/companies/*') && tab.title === 'Pay Plus Admin') {
-//             const urlArr = Array.from(tab.url);
-//             if(urlArr.slice(33,69).join('').length === 36) {
-//                 const editView = tab.url.includes('edit');
-//                 chrome.scripting.executeScript({
-//                     target: { tabId: tab.id },
-//                     args: [editView],
-//                     func: setTitle,
-//                 });
-//             }
-//         }
-//     }
-// });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: setTitle,
+        });
+    }
+});
