fc55562827572d63d839c539f543de12a37cc9cd
yoadmash <yoad.mashiach2@gmail.com>
2023-04-11 02:31:01 +0300

    fixed search

--- tab renamer/interface/script.js
+++ tab renamer/interface/script.js
@@ -255,7 +255,6 @@ async function search() {
                 } else {
                     renderWindows(windows);
                 }
-                await updateActiveWindowAndTab();
             }
         });
     });
